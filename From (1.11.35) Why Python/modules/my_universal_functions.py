839ada561a1337f695e64a1d6608598b12e549d4
Wasima Ibnat <wasima1412@gmail.com>
2024-01-15 20:57:22 +0600

    Folders added!

---
+++ From (1.11.35) Why Python/modules/my_universal_functions.py
@@ -0,0 +1,14 @@
+def checkIfNotNumeric(*args):    
+    for x in args:
+        if not(isinstance(x,(int,float))):
+            return False
+    return True
+
+def addAllNumerics(*args):
+    s = 0
+    for x in args:
+        s+=x
+    return s
+
+myName = "Python Course"
+    
